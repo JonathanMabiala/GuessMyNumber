ec785fa5772bf757a644e22f46f8b6862fae3937
JonathanMabiala <62263731+JonathanMabiala@users.noreply.github.com>
2023-12-29 19:18:05 -0400

    initial code

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { View, TextInput, Pressable } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      BUTTONS
+    </View>
+  );
+};
+
+export default StartGameScreen;
